0f50b3d0a70cecea699d6b7ed8d37621be512c1b
Vy Le <vy.t.le@colorado.edu>
2018-03-12 12:49:32 -0600

    added js files in head

---
+++ Project/FireBaseInit.js
@@ -0,0 +1,14 @@
+(function() {
+// Initialize Firebase
+
+var config = {
+	apiKey: "AIzaSyCcPhzOSSNKeB8tUIpBxgpgiN3qByeg2dY",
+	authDomain: "snowsafe-b378c.firebaseapp.com",
+	databaseURL: "https://snowsafe-b378c.firebaseio.com",
+	projectId: "snowsafe-b378c",
+	storageBucket: "",
+	messagingSenderId: "676990316274"
+};
+firebase.initializeApp(config);
+  	  		
+}());
